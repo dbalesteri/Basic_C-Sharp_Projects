0d6eee31fac510ad6717ebec4b5ea9ea7f53977e
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-11 14:18:11 -0700

    Add files via upload

---
+++ Daily Report Console App/Daily Report Console App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Daily_Report_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("");
+            Console.WriteLine("What is your name?");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool helpNeeded = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
