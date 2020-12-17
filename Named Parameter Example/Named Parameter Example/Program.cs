ff7c2ff3ba53a3b8012369689412b79ce97e7364
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-17 14:23:51 -0700

    Add files via upload

---
+++ Named Parameter Example/Named Parameter Example/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Parameter_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exampleClass exampleClass = new exampleClass();
+            bool validAnswer = false;
+
+            while (!validAnswer)
+            {
+                try
+                {
+                    Console.WriteLine("Enter in a whole number:");
+                    int input1 = Convert.ToInt32(Console.ReadLine());
+                    bool validAnswer2 = false;
+                    while (!validAnswer2)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Enter in another whole number:");
+                            int input2 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("The output is:");
+                            exampleClass.voidMethod(num1: input1, num2: input2);
+                            validAnswer2 = true;
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine("Invalid input. Enter a whole number:");
+                        }
+                        catch (OverflowException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine("Enter a number 1073741824 or less:");
+                        }
+                    }
+                    validAnswer = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Invalid input. Enter a whole number:");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Enter a number 1073741824 or less:");
+                }
+            }
+            Console.WriteLine("That's it, press enter to exit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Named Parameter Example/Named Parameter Example/exampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Parameter_Example
+{
+    class exampleClass
+    {
+        public void voidMethod (int num1, int num2)
+        {
+            num1 = num1 + 23;
+            Console.WriteLine(num2);
+        }
+    }
+}
