1791c6488e9a8dbcb38b9ba3daa782fa64ef7e9b
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-19 12:31:31 -0700

    Add files via upload

---
+++ Inheritance Example/Inheritance Example/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Example
+{
+    public class Employee : Person
+    {
+        int Id { get; set; }
+    }
+}
