8ec941d83f8c08e389c8d9f357967d9c35407b6f
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-19 17:50:45 -0700

    Add files via upload

---
+++ Interface Example/Interface Example/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Example
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Hey boss,\nI quit.\nSincerely,\n" + firstName + " " + lastName);
+        }
+    }
+}
