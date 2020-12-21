3755f0e88c65417b836a7771dea216535a60b2ea
Daniel Balesteri <balesteridan@gmail.com>
2020-12-20 18:21:35 -0700

    fixed interface project upload
    was missing files

---
+++ Interface Example/Interface Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee = new Employee() { firstName = "John", lastName = "Doe" };
+            employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
