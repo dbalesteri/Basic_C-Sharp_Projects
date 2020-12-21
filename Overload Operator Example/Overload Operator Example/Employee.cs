3a3a79caede9ca8d8c57ac42bcbd13d1d6a6b15d
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-21 10:58:26 -0700

    Add files via upload

---
+++ Overload Operator Example/Overload Operator Example/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator_Example
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Hey boss,\nI quit.\nSincerely,\n" + firstName + " " + lastName);
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool duplicate;
+            if (employee1.Id == employee2.Id)
+            {
+                duplicate = true;
+            }
+            else
+            {
+                duplicate = false;
+            }
+            return duplicate;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool duplicate;
+            if (employee1.Id != employee2.Id)
+            {
+                duplicate = false;
+            }
+            else
+            {
+                duplicate = true;
+            }
+            return duplicate;
+        }
+    }
+}
---
+++ Overload Operator Example/Overload Operator Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { firstName = "Sandy", lastName = "Sanguine", Id = 3 };
+            Employee employee2 = new Employee() { firstName = "Luigi", lastName = "Marinara", Id = 3 };
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("These employees have matching Ids.");
+            }
+            else
+            {
+                Console.WriteLine("The employees do not have matching Ids.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
