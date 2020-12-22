92b923cddef330f25c15e2ffebe88de1eb00d794
Dan Balesteri <63321771+dbalesteri@users.noreply.github.com>
2020-12-22 11:47:47 -0700

    Add files via upload

---
+++ Enum Example/Enum Example/Day.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Example
+{
+    public class Day
+    {
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
---
+++ Enum Example/Enum Example/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week:");
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+                try
+                {
+                    string userInput = Console.ReadLine();
+                    userInput = char.ToUpper(userInput[0]) + userInput.Substring(1);
+                    bool validOption = Enum.IsDefined(typeof(DayOfWeek), userInput);
+                    if (!validOption)
+                    {
+                        Console.WriteLine("Please enter an acutal day of the week:");
+                        continue;
+                    }
+                    DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userInput);
+                    Console.WriteLine("You entered: " + day);
+                    validAnswer = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an acutal day of the week:");
+                }
+            }
+            Console.WriteLine("That's all, thank you.");
+            Console.ReadLine();
+        }
+    }
+}
