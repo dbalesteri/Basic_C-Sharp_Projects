c62d874d60e9cf84e0951c4950eaf9027469f076
Daniel Balesteri <balesteridan@gmail.com>
2021-01-12 09:38:38 -0700

    add code first example
    upload to repo

---
+++ Code_First_Example/Code_First_Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Code_First_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                Console.WriteLine("Student Database\n\nEnter in the name of a new student:");
+                string sName = Console.ReadLine();
+                var student = new Student() { StudentName = sName };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+                Console.WriteLine(sName + " has been added. Thank you.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
