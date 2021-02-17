7cda598f5763097bdfb2e5a7d0b8f7c232402df1
Daniel Balesteri <balesteridan@gmail.com>
2021-02-16 21:13:15 -0700

    Add ContosoUniversity
    Add contoso university tutorial project

---
+++ ContosoUniversity/ContosoUniversity/Migrations/202102170256512_UpdateRelatedData.cs
@@ -0,0 +1,16 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateRelatedData : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; } //foreign key, correspoind navigation property is Course
+        public int StudentID { get; set; } //foreign key, corresponding navigation property is Student
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; } // ? indicates nullable
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
